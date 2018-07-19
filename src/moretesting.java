7213d6e8361131b72ec105775baa8ae0b3f338af
Sam1370 <sampiano@outlook.com>
2018-07-18 18:58:10 -0700

    Delete moretesting.java

--- src/moretesting.java
+++
@@ -1,7 +0,0 @@
-
-public class moretesting {
-	public static void main(String[] args) {
-		Base alphabet = new Base("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-		System.out.println(Base.TEN.convertTo(alphabet, "1000"));
-	}
-}
